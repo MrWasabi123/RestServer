2840445b9fc18870f548158c87bb5fd583ed67d1
Wanja <w.sajko@gmail.com>
2018-12-29 16:50:28 +0100

    test

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+    	//trying to update the git version
         SpringApplication.run(Application.class, args);
     }
 }
