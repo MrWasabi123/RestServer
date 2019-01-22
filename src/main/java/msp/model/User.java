0614ee53bc69e67fe9fc8121980015e6b3312fdd
Wanja <w.sajko@gmail.com>
2019-01-22 13:45:47 +0100

    a

--- src/main/java/msp/model/User.java
+++ src/main/java/msp/model/User.java
@@ -2,13 +2,22 @@ package msp.model;
 
 
 import java.io.Serializable;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "user")
@@ -29,7 +38,7 @@ public class User implements Serializable{
 	@Column(name = "studies")
     private String studies;
 	@Column(name = "subject")
-    private String subject;
+	private String subject;
 	@Column(name = "plan")
     private String plan;
 	@Column(name = "ratings")
@@ -40,7 +49,29 @@ public class User implements Serializable{
     private String userRole;
     @Column(name = "firebase_id")
     private String firebaseId;
+    @Column(name = "imageURL")
+    private String imageURL;
+    
+   
+	@ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_lecture", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "user_id"), inverseJoinColumns = @JoinColumn(name = "lecture_id", referencedColumnName = "lecture_id"))
+    @JsonIgnoreProperties("users")
+    private Set<Lecture> lectures;
+    
+    @JsonIgnoreProperties("user")
+    @OneToMany(mappedBy = "user", fetch=FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Rating> userRatings;
+    
+    @JsonIgnoreProperties("author")
+    @OneToMany(mappedBy = "author", fetch=FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Rating> yourRatings;
+    
+    @JsonIgnoreProperties("appointmentUser")
+    @OneToMany(mappedBy = "appointmentUser", fetch=FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Appointment> appointments;
+    
 
+    
     public User(){
 
     }
@@ -70,8 +101,17 @@ public class User implements Serializable{
 		this.userRole = userRole;
 	}
 
+	
     
-    public long getId() {
+    public Set<Appointment> getAppointments() {
+		return appointments;
+	}
+
+	public void setAppointments(Set<Appointment> appointments) {
+		this.appointments = appointments;
+	}
+
+	public long getId() {
         return id;
     }
 
@@ -118,6 +158,30 @@ public class User implements Serializable{
     public void setSubject(String subject) {
         this.subject = subject;
     }
+    
+    public Set<Lecture> getLectures() {
+        return this.lectures;
+    }
+
+    public void setLectures(Set<Lecture> lectures) {
+        this.lectures = lectures;
+    }
+    
+    public Set<Rating> getYourRatings() {
+        return this.yourRatings;
+    }
+
+    public void setYourRatings(Set<Rating> yourRatings) {
+        this.yourRatings = yourRatings;
+    }
+    
+    public Set<Rating> getUserRatings() {
+        return this.userRatings;
+    }
+
+    public void setUserRatings(Set<Rating> userRatings) {
+        this.userRatings = userRatings;
+    }
 
     public String getStudies() {
         return studies;
@@ -155,7 +219,6 @@ public class User implements Serializable{
 		this.enabled = enabled;
 	}
 
-	//@OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
 	public String getUserRole() {
 		return this.userRole;
 	}
@@ -171,6 +234,15 @@ public class User implements Serializable{
 	public void setFirebaseId(String firebaseId) {
 		this.firebaseId = firebaseId;
 	}
+	
+	public String getImageURL() {
+		return imageURL;
+	}
+
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+
     
     @Override
     public String toString() {
